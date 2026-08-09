406f407d0d5729bb7d2a48a109aa886fe89c2e71
MenuMinyu <130168857+MenuMinyu@users.noreply.github.com>
2026-08-09 20:43:27 +0100

    Latest code

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Food from "./Food";
+import Button from "./Button";
+
+function App() {
+
+  return(
+    <>
+      <Header/>
+      <Food/>
+      <Button/>
+      <Footer/> 
+    </>
+  );
+
+}
+
+export default App
---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy;2026 Your website name</p>
+        </footer>
+    )
+}
+
+export default Footer
